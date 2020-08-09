f38662a241356057a00380dc2ea01154987c91d3
Ibara <ibara1454@gmail.com>
2020-08-09 20:43:09 +0900

    :heavy_plus_sign: add portal-vue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
+import PortalVue from 'portal-vue';
 import App from './App.vue';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueCompositionAPI);
+// Portal-Vue
+// https://github.com/LinusBorg/portal-vue
+Vue.use(PortalVue);
 
 new Vue({
   render: (h) => h(App),
