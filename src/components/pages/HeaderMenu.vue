b8d0cf5eb79d93ec8a083929a1274a1e4bc104ed
Ibara <ibara1454@gmail.com>
2020-08-09 22:04:38 +0900

    :sparkles: show active device if exists

--- src/components/pages/HeaderMenu.vue
+++ src/components/pages/HeaderMenu.vue
@@ -3,8 +3,16 @@
     <MainLogo />
 
     <FlatButton :class="[$style.button]">
-      <SmallIcon :icon="faPlug" />
-      <p>No device</p>
+      <!-- Shows "NO DEVICE" when no device detected. -->
+      <template v-if="state.activeDevice === undefined">
+        <SmallIcon :icon="faPlug" />
+        <p>No device</p>
+      </template>
+      <!-- Otherwise, shows the device name -->
+      <template v-else>
+        <img :class="$style.keyboard" :src="KeyboardIcon" />
+        <p>{{ state.activeDevice.name }}</p>
+      </template>
     </FlatButton>
 
     <FlatButton :class="[$style.button]">
@@ -22,6 +30,7 @@ import FlatButton from '@/components/atoms/FlatButton.vue';
 import SmallIcon from '@/components/atoms/SmallIcon.vue';
 import { faPlug, faCog } from '@fortawesome/free-solid-svg-icons';
 import MainLogo from '@/components/molecules/MainLogo.vue';
+import KeyboardIcon from '@/assets/images/keyboard.svg';
 
 export default defineComponent({
   name: 'HeaderMenu',
@@ -33,12 +42,15 @@ export default defineComponent({
   setup() {
     const store = inject(MIDIDeviceStore.key) as MIDIDeviceStore;
     const { state } = store;
-    return { state, faPlug, faCog };
+    return {
+      state, faPlug, faCog, KeyboardIcon,
+    };
   },
 });
 </script>
 
 <style lang="scss" module>
+@import "~@/components/styles/colors";
 
 .flex {
   display: flex;
@@ -59,6 +71,10 @@ export default defineComponent({
 
 .button > * {
   padding: 0 5px;
+}
+
+.keyboard {
+  height: 1.5em;
 }
 
 </style>
